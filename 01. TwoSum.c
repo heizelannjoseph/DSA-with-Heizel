bb2b978dfcec7842073f414e3e8b42a8a1444bc3
Heizel Ann Joseph <heizelannjoseph@gmail.com>
2025-08-05 22:56:59 +0530

    Update 01. TwoSum.c

--- 01. TwoSum.c
+++ 01. TwoSum.c
@@ -13,9 +13,9 @@
 //
 // Language: C
 
-#include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {
@@ -30,45 +30,4 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     }
     *returnSize = 0;
     return NULL;
-}
-
-int main() {
-    int numsSize, target;
-
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &numsSize);
-
-    if (numsSize < 2) {
-        printf("Array must have at least 2 elements.\n");
-        return 1;
-    }
-
-    int* nums = (int*)malloc(numsSize * sizeof(int));
-    if (nums == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
-
-    printf("Enter %d integers:\n", numsSize);
-    for (int i = 0; i < numsSize; i++) {
-        scanf("%d", &nums[i]);
-    }
-
-    printf("Enter the target sum: ");
-    scanf("%d", &target);
-
-    int returnSize;
-    int* result = twoSum(nums, numsSize, target, &returnSize);
-
-    if (result != NULL && returnSize == 2) {
-        printf("Indices: [%d, %d]\n", result[0], result[1]);
-        printf("Values: [%d, %d]\n", nums[result[0]], nums[result[1]]);
-        free(result);
-    } else {
-        printf("No valid pair found.\n");
-    }
-
-    free(nums);
-
-    return 0;
 }
