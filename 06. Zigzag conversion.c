9989dcb4b2e3c0fba81fe982676a902552c60e94
Heizel Ann Joseph <heizelannjoseph@gmail.com>
2025-08-05 22:51:21 +0530

    Update 06. Zigzag conversion.c

--- 06. Zigzag conversion.c
+++ 06. Zigzag conversion.c
@@ -10,46 +10,49 @@ s consists of English letters (lower-case and upper-case), ',' and '.'.
 1 <= numRows <= 1000
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 char* convert(char* s, int numRows) {
     if (numRows == 1 || numRows >= strlen(s)) {
-        char* result = (char*)malloc(strlen(s) + 1);
-        strcpy(result, s);
-        return result;
-    }
-
-    char** rows = (char**)malloc(numRows * sizeof(char*));
-    int* lens = (int*)calloc(numRows, sizeof(int));
-
-    for (int i = 0; i < numRows; i++) {
-        rows[i] = (char*)malloc(strlen(s) + 1);
-    }
-
-    int row = 0;
-    int goingDown = 0;
-
-    for (int i = 0; s[i] != '\0'; i++) {
-        rows[row][lens[row]++] = s[i];
-        if (row == 0 || row == (numRows - 1)) {
-            goingDown = !goingDown;
-        }
-        row += goingDown ? 1 : -1;
-    }
-
     char* result = (char*)malloc(strlen(s) + 1);
-    int pos = 0;
+    strcpy(result, s);
+    return result;
+}
 
-    for (int i = 0; i < numRows; i++) {
-        for (int j = 0; j < lens[i]; j++) {
-            result[pos++] = rows[i][j];
+
+    char** rows=(char**)malloc(numRows * sizeof(char*));
+    int* lens=(int*)calloc(numRows, sizeof(int));
+
+    for(int i=0;i<numRows;i++)
+    {
+        rows[i]=(char*)malloc(strlen(s)+1);
+    }
+
+    int row=0;
+    int goingDown=0;
+
+    for(int i=0;s[i]!='\0';i++)
+    {
+        rows[row][lens[row]++]=s[i];
+        if(row==0 || row ==(numRows-1))
+        {
+            goingDown=!goingDown;
+        }
+        row+=goingDown? 1 : -1;
+    }
+
+    char* result=(char*)malloc(strlen(s)+1);
+    int pos=0;
+
+    for(int i=0;i<numRows;i++)
+    {
+        for(int j=0;j<lens[i];j++)
+        {
+            result[pos++]=rows[i][j];
         }
     }
-    result[pos] = '\0';
+    result[pos]='\0';
 
-    for (int i = 0; i < numRows; i++) {
+    for(int i=0;i<numRows;i++)
+    {
         free(rows[i]);
     }
 
@@ -57,13 +60,4 @@ char* convert(char* s, int numRows) {
     free(lens);
 
     return result;
-}
-
-int main() {
-    char s[] = "PAYPALISHIRING";
-    int numRows = 3;
-    char* result = convert(s, numRows);
-    printf("%s\n", result);
-    free(result);
-    return 0;
 }
