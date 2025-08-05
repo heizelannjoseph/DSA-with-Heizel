2f2b49a23172259088fa39ea2195d25b90aea18f
Heizel Ann Joseph <heizelannjoseph@gmail.com>
2025-08-05 22:52:26 +0530

    Update 05. LongestPalindromicSubstring.c

--- 05. LongestPalindromicSubstring.c
+++ 05. LongestPalindromicSubstring.c
@@ -12,59 +12,44 @@
  ============================================================================
 */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
 char* longestPalindrome(char* s) {
-    int len = strlen(s);
-    if (len < 2) {
+    int len=strlen(s);
+    if(len<2)
+    {
         return s;
     }
+    int start=0, maxLen=1; 
 
-    int start = 0, maxLen = 1;
-
-    for (int i = 0; i < len; i++) {
-        int left = i, right = i;
-        while (left >= 0 && right < len && s[left] == s[right]) {
-            if ((right - left + 1) > maxLen) {
-                start = left;
-                maxLen = right - left + 1;
-            }
+        for(int i=0;i<len;i++)
+        {
+            int left=i, right=i;
+            while(left>=0 && right<len && s[left]==s[right])
+            {
+                if((right-left+1)>maxLen)
+                {
+                    start=left;
+                    maxLen=right-left+1;
+                }
             left--;
             right++;
-        }
-
-        left = i;
-        right = i + 1;
-        while (left >= 0 && right < len && s[left] == s[right]) {
-            if ((right - left + 1) > maxLen) {
-                start = left;
-                maxLen = right - left + 1;
             }
+            
+
+            left=i, right=i+1;
+            while(left>=0 && right<len && s[left]==s[right])
+            {
+                if((right-left+1)>maxLen)
+                {
+                    start=left;
+                    maxLen=right-left+1;
+                }
             left--;
             right++;
+            }
         }
-    }
 
-    char* res = (char*)malloc((maxLen + 1) * sizeof(char));
-    strncpy(res, s + start, maxLen);
-    res[maxLen] = '\0';
+    char* res=(char*)malloc((maxLen+1)*sizeof(char));
+    strncpy(res,s+start,maxLen);
+    res[maxLen]='\0';
     return res;
-}
-
-int main() {
-    char s[1001];
-
-    printf("Enter a string (only letters and digits): ");
-    scanf("%1000s", s);
-
-    char* result = longestPalindrome(s);
-    printf("Longest Palindromic Substring: %s\n", result);
-
-    if (result != s) {
-        free(result);
-    }
-
-    return 0;
 }
