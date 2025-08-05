3184088004e9adcd03f3b056eaae8a9174ba6df9
Heizel Ann Joseph <heizelannjoseph@gmail.com>
2025-08-05 22:55:04 +0530

    Update 03. LongestSubstring.c

--- 03. LongestSubstring.c
+++ 03. LongestSubstring.c
@@ -8,24 +8,18 @@
  * - s consists of English letters, digits, symbols, and spaces.
  */
 
-#include <stdio.h>
-#include <string.h>
-
 int lengthOfLongestSubstring(char* s) {
-    int lastIndex[256]; 
+    int lastIndex[256];  
     for (int i = 0; i < 256; i++) lastIndex[i] = -1;
 
     int maxLen = 0;
     int start = 0;  
 
     for (int end = 0; s[end] != '\0'; end++) {
-        unsigned char currentChar = (unsigned char)s[end];
-
-        if (lastIndex[currentChar] >= start) {
-            start = lastIndex[currentChar] + 1;  
+        if (lastIndex[(unsigned char)s[end]] >= start) {
+            start = lastIndex[(unsigned char)s[end]] + 1;
         }
-
-        lastIndex[currentChar] = end;  
+        lastIndex[(unsigned char)s[end]] = end;
         int windowLen = end - start + 1;
         if (windowLen > maxLen) {
             maxLen = windowLen;
@@ -33,22 +27,4 @@ int lengthOfLongestSubstring(char* s) {
     }
 
     return maxLen;
-}
-
-void test(char* input) {
-    int result = lengthOfLongestSubstring(input);
-    printf("Input: \"%s\"\n", input);
-    printf("Output: %d\n", result);
-    printf("-----------------------\n");
-}
-
-int main() {
-    test("abcabcbb");  
-    test("bbbbb");    
-    test("pwwkew");   
-    test("");          
-    test("aab");      
-    test("dvdf");      
-
-    return 0;
 }
