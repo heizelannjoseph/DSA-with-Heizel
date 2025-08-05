8778347f96d26ef83e554d8b078800b5caa3baf7
Heizel Ann Joseph <heizelannjoseph@gmail.com>
2025-08-05 22:48:42 +0530

    Update 11. Container with most water.c

--- 11. Container with most water.c
+++ 11. Container with most water.c
@@ -22,39 +22,28 @@ n == height.length
 0 <= height[i] <= 10^4
 */
 
-#include <stdio.h>
-
 int maxArea(int* height, int heightSize) {
-    int max = 0;
-    int left = 0;
-    int right = heightSize - 1;
+    int max=0;
+    int left=0;
+    int right=heightSize-1;
 
-    while (left < right) {
-        int h = height[left] < height[right] ? height[left] : height[right];
-        int width = right - left;
-        int area = width * h;
+    while(left<right)
+    {
+        int h=height[left]<height[right]? height[left] : height[right];
+        int width=right-left;
+        int area=width*h;
 
-        if (area > max) {
-            max = area;
+        if(area>max)
+        {
+            max=area;
         }
 
-        if (height[left] < height[right]) {
+        if(height[left]<height[right])
+        {
             left++;
-        } else {
+        }else{
             right--;
         }
     }
     return max;
-}
-
-int main() {
-    int height[] = {1,8,6,2,5,4,8,3,7};
-    int size = sizeof(height) / sizeof(height[0]);
-    printf("Max Area: %d\n", maxArea(height, size));
-
-    int height2[] = {1,1};
-    int size2 = sizeof(height2) / sizeof(height2[0]);
-    printf("Max Area: %d\n", maxArea(height2, size2));
-
-    return 0;
 }
