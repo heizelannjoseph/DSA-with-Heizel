ec4830e766d2ea1a418ac8f4ee5c1df89b17cca4
Heizel Ann Joseph <heizelannjoseph@gmail.com>
2025-08-07 23:56:56 +0530

    Create 15. 3Sum.c

---
+++ 15. 3Sum.c
@@ -0,0 +1,48 @@
+int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = 0; j < numsSize - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = temp;
+            }
+        }
+    }
+
+    int** result = (int**)malloc(sizeof(int*) * 100000);
+    *returnColumnSizes = (int*)malloc(sizeof(int) * 100000);
+    int count = 0;
+
+    for (int i = 0; i < numsSize - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        int left = i + 1;
+        int right = numsSize - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum == 0) {
+                result[count] = (int*)malloc(sizeof(int) * 3);
+                result[count][0] = nums[i];
+                result[count][1] = nums[left];
+                result[count][2] = nums[right];
+                (*returnColumnSizes)[count] = 3;
+                count++;
+
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    *returnSize = count;
+    return result;
+}
