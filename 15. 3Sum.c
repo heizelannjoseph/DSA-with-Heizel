30e322b1e330f0ebae7cea776af5673cfd9eca6a
Heizel Ann Joseph <heizelannjoseph@gmail.com>
2025-08-07 23:58:33 +0530

    Update 15. 3Sum.c

--- 15. 3Sum.c
+++ 15. 3Sum.c
@@ -13,7 +13,7 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
     *returnColumnSizes = (int*)malloc(sizeof(int) * 100000);
     int count = 0;
 
-    for (int i = 0; i < numsSize - 2; i++) {
+    for (int i = 0; i < numsSize - 2; i++) { 
         if (i > 0 && nums[i] == nums[i - 1]) continue;
 
         int left = i + 1;
